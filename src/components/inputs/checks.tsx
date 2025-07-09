f36a715748f2154ea4272a6155d076de43a53661
pigson <stalk9424@gmail.com>
2025-07-10 00:35:40 +0200

    .gitignore 	.vscode/ 	next-env.d.ts 	next.config.js 	package-lock.json 	package.json 	plugins/ 	postcss.config.cjs 	src/TailwindSafeClasses.tsx 	src/app.tsx 	src/components/acordeon.tsx 	src/components/alert.tsx 	src/components/app-bar/ 	src/components/breadcrumbs.tsx 	src/components/carousel/ 	src/components/data-table.tsx 	src/components/divider.tsx 	src/components/error.tsx 	src/components/form/ 	src/components/hooks/ 	src/components/inputs/atomize.tsx 	src/components/inputs/autocomplete.tsx 	src/components/inputs/base.tsx 	src/components/inputs/button-filtre-group.tsx 	src/components/inputs/button-group.tsx 	src/components/inputs/color.tsx 	src/components/inputs/file.tsx 	src/components/inputs/hooks.ts 	src/components/inputs/index.tsx 	src/components/inputs/number.tsx 	src/components/inputs/radio.tsx 	src/components/inputs/select.tsx 	src/components/inputs/slider.tsx 	src/components/inputs/styles/ 	src/components/inputs/switch.tsx 	src/components/inputs/text-area.tsx 	src/components/inputs/type.ts 	src/components/list/ 	src/components/nav-bars/ 	src/components/text/ 	src/components/theme/ 	src/components/tools/ 	src/editor/ 	src/index.html 	src/index.ts 	src/style/ 	tailwind.config.js 	tsconfig.json 	vite.config.js

---
+++ src/components/inputs/checks.tsx
@@ -0,0 +1,86 @@
+import type { CheckBoxInputProps } from './type';
+import { FormWrapper } from './atomize';
+import clsx from 'clsx';
+
+
+
+export default function CheckBoxInput({ 
+    onChange, 
+    size, 
+    color, 
+    value, 
+    type, 
+    style,
+    className,
+    ...props 
+}: CheckBoxInputProps) {
+    const sizes = {
+        xs: 'w-4 h-4',
+        sm: 'w-5 h-5',
+        md: 'w-6 h-6',
+        lg: 'w-6 h-6',
+        xl: 'w-7 h-7',
+        auto: 'w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 lg:w-6 lg:h-6 xl:w-7 xl:h-7'
+    }
+    const top = {
+        xs: 'top-1',
+        sm: 'top-1',
+        md: 'top-2',
+        lg: 'top-2',
+        xl: 'top-2',
+        auto: 'top-1 sm:top-1 md:top-2 lg:top-2 xl:top-2'
+    }
+
+
+    return (
+        <FormWrapper 
+            size={size} 
+            disabledVisibility
+            { ...props }
+        >
+            <label className={`relative inline-flex ${top[size]??top.auto} items-center cursor-pointer`}>
+                <input key={value ? 1 : 0} defaultChecked={value} type="checkbox" className="sr-only peer" />
+                
+                {/* обводка */}
+                <div 
+                    style={style}
+                    className={`
+                        ${sizes[size] ?? sizes.auto}
+                        border-1
+                        border-[${style?.borderColor ?? 'var(--input-color)'}]
+                        opacity-20
+                        rounded
+                        transition-all
+                        duration-200
+                        peer-checked:opacity-100
+                        flex items-center justify-center
+                    `}
+                >
+                </div>
+
+                {/* галочка */}
+                <svg
+                    className={`
+                        absolute 
+                        left-0 top-0 
+                        ${sizes[size] ?? sizes.auto} 
+                        p-[2px]
+                        text-[var(--input-color)]
+                        pointer-events-none
+                        opacity-0
+                        scale-75
+                        peer-checked:opacity-100 
+                        peer-checked:scale-100
+                        transition-all duration-200
+                    `}
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="3"
+                    viewBox="0 0 24 24"
+                >
+                    <path d="M5 13l4 4L19 7" />
+                </svg>
+            </label>
+        </FormWrapper>
+    );
+}
